033d8744d20eb630190ae60ada1b2d8b1f2f9569
NGAN PHAM <nganvpham@gmail.com>
2018-01-28 01:25:01 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,6 +16,12 @@ function whileLoop(number){
   return 'done';
 }
 
-function doWhileLoop(array){
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
 
+function doWhileLoop(array){
+  do{
+
+  } while (maybeTrue(()));
 }
