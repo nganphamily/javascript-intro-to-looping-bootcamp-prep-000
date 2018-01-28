bbe75c2737c2ff2ccef26424840fad37fde2f2e9
NGAN PHAM <nganvpham@gmail.com>
2018-01-28 01:22:04 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,17 @@
+function forLoop (array){
+  for(let i=0; i<25; i++){
+    if (i===1){
+      array.push("I'm 1 strang loop.");
+    } else {
+      array.push("I am ${i} strange loops.");
+    }
+  }
+  return array;
+}
+
+function whileLoop(number){
+  while (number > 0){
+    console.log(--number);
+  }
+  return 'done';
+}
